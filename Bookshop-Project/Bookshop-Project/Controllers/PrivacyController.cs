44c7f76070383ee2c1f3a5352af0a353926842dd
TheoE23 <tedo_enchev@mail.bg>
2023-06-13 04:05:21 +0300

    Product cards and layout changes

---
+++ Bookshop-Project/Bookshop-Project/Controllers/PrivacyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshop_Project.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
